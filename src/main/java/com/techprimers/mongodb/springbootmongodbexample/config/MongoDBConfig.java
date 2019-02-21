be875949a420dbc66ddf756a32eaf826b20bbd81
Sourabh Gupta <gupta.sourabh385@gmail.com>
2019-02-21 23:06:16 +0530

    Adding new user

--- src/main/java/com/techprimers/mongodb/springbootmongodbexample/config/MongoDBConfig.java
+++ src/main/java/com/techprimers/mongodb/springbootmongodbexample/config/MongoDBConfig.java
@@ -18,6 +18,7 @@ public class MongoDBConfig {
             //userRepository.save(new Users(1, "Peter", "Development", 3000L));
             //userRepository.save(new Users(2, "Sam", "Operations", 2000L));
             userRepository.save(new Users(3, "Sourabh", "DevOps", 2000L, "Male"));
+            userRepository.save(new Users(4, "Lohit", "DevOps", 2000L, "Male"));
         };
     }
 
